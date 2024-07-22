a160f0e2557344e614a7e648f25d40caaa2efb96
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2024-07-22 00:21:11 -0500

    Added work count to both entries and input panel; implemented search function for keywords or date

--- src/components/Journals.tsx
+++ src/components/Journals.tsx
@@ -9,12 +9,35 @@ const Journals = () => {
     const [disabled, setDisabled] = useState(true);
     const [content, setContent] = useState("");
     const [journals, setJournals] = useState<Journal[]>([]);
+    const [filteredJournals, setFilteredJournals] = useState<Journal[]>([]);
+    const [filter, setFilter] = useState("");
 
-    const fetchJournals = () => api.journals.load().then(setJournals);
+    const fetchJournals = () =>
+        api.journals.load().then((jrnls) => {
+            setJournals(jrnls);
+            setFilteredJournals(jrnls);
+        });
 
     useEffect(() => {
         setDisabled(content.length < 12);
     }, [content]);
+
+    useEffect(() => {
+        if (filter === "") {
+            setFilteredJournals(journals);
+            return;
+        }
+
+        const filtered = journals.filter((j) => {
+            const contentMatches = j.content.toLowerCase().includes(filter.toLowerCase());
+            const dateMatches = new Date(j.created_at).toDateString().toLowerCase().includes(filter.toLowerCase());
+
+            console.log({ contentMatches, dateMatches, j });
+
+            return contentMatches || dateMatches;
+        });
+        setFilteredJournals(filtered);
+    }, [filter]);
 
     useEffect(() => {
         fetchJournals();
@@ -47,6 +70,12 @@ const Journals = () => {
                 });
             }
         });
+    };
+
+    const getWords = (string: string) => {
+        const count = string.split(" ").length;
+
+        return count === 1 ? `${count} word` : `${count} words`;
     };
 
     return (
@@ -86,6 +115,7 @@ const Journals = () => {
                     <span style={{ display: "block", marginTop: "18px" }}>
                         {content.length.toLocaleString()} / {MAX_CONTENT_LENGTH.toLocaleString()}
                     </span>
+                    <span style={{ display: "block", marginTop: "3px" }}>{getWords(content)}</span>
                 </button>
             </div>
 
@@ -93,6 +123,7 @@ const Journals = () => {
                 style={{
                     display: "flex",
                     flexWrap: "wrap",
+                    justifyContent: "center",
                     marginTop: "5%",
                     borderRadius: "12px",
                     backgroundColor: "white",
@@ -101,30 +132,49 @@ const Journals = () => {
                     height: "60vh",
                 }}
             >
-                {journals.map((j) => (
-                    <div style={{ width: "80%", border: "2px solid black", borderRadius: "12px", margin: "4%", backgroundColor: "white" }}>
-                        <h2 style={{ color: "black" }}>
-                            {new Date(j.created_at).toLocaleDateString()}{" "}
-                            <span
-                                style={{ marginLeft: "15px" }}
-                                onClick={() => handleDelete(j._id)}
-                            >
-                                ❌
-                            </span>
-                        </h2>
-                        <div>
-                            {j.content.split("\n").map((line, i) => (
-                                <p
-                                    style={{ color: "black" }}
-                                    key={`j-${j._id}-p-${i}`}
+                <div>
+                    <span style={{ fontSize: "1.85rem", marginRight: "10px" }}>🔎</span>
+                    <input
+                        style={{
+                            fontSize: "1.85rem",
+                            marginTop: "5%",
+                            borderRadius: "12px",
+                        }}
+                        type="search"
+                        value={filter}
+                        onChange={(e) => setFilter(e.target.value)}
+                    />
+                    {filter && <p style={{ color: "black" }}>Search results for "{filter}":</p>}
+                </div>
+
+                <>
+                    {filteredJournals.map((j) => (
+                        <div style={{ width: "80%", border: "2px solid black", borderRadius: "12px", margin: "4%", backgroundColor: "white" }}>
+                            <h2 style={{ color: "black" }}>
+                                {new Date(j.created_at).toDateString()}{" "}
+                                <span
+                                    style={{ marginLeft: "15px" }}
+                                    onClick={() => handleDelete(j._id)}
                                 >
-                                    {line}
-                                </p>
-                            ))}
+                                    ❌
+                                </span>
+                            </h2>
+                            <div>
+                                {j.content.split("\n").map((line, i) => (
+                                    <p
+                                        style={{ color: "black" }}
+                                        key={`j-${j._id}-p-${i}`}
+                                    >
+                                        {line}
+                                    </p>
+                                ))}
+                            </div>
+                            <p style={{ color: "black" }}>
+                                {getWords(j.content)}. Posted {new Date(j.created_at).toLocaleTimeString()}
+                            </p>
                         </div>
-                        <p style={{ color: "black" }}>{new Date(j.created_at).toLocaleTimeString()}</p>
-                    </div>
-                ))}
+                    ))}
+                </>
             </div>
         </div>
     );
